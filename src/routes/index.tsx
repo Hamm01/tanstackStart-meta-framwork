ee57d89761075e6ba49849015c33509b366df8fb
Himanish kochhar <himanish.kochhar@gmail.com>
2025-09-27 05:58:17 +0530

    TEst route created as created a test folder

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,17 @@
+import { db } from '@/db'
 import { createFileRoute } from '@tanstack/react-router'
 
 
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute('/')({
+  component: App,
+  loader: () => {
+    return db.query.todos.findMany()
+  },
+})
 
 function App() {
+  const todos = Route.useLoaderData()
 
-  return null
+  return <h1>hi {JSON.stringify(todos[0].name.trim(), null, 2)}</h1>
 
 }
