76d741ce9c39485ec62f0230579d4de7a86f7e63
Himanish kochhar <himanish.kochhar@gmail.com>
2025-10-06 11:02:34 +0530

    update todo server function added with todo to update and redirect back to home

--- src/components/todo-form.tsx
+++ src/components/todo-form.tsx
@@ -8,6 +8,7 @@ import { redirect } from "@tanstack/react-router";
 import z from 'zod'
 import { todos } from "@/db/schema";
 import { db } from "@/db";
+import { eq } from "drizzle-orm";
 
 const addTodo = createServerFn({ method: 'POST' }).inputValidator(z.object({ name: z.string().min(1) })).handler(async ({ data }) => {
     await db.insert(todos).values({ ...data, isComplete: false })
@@ -15,25 +16,50 @@ const addTodo = createServerFn({ method: 'POST' }).inputValidator(z.object({ nam
 })
 
 
+const updateTodo = createServerFn({ method: 'POST' }).inputValidator(z.object({ id: z.string().min(1), name: z.string().min(1) })).handler(async ({ data }) => {
 
-export function TodoForm() {
+    await db.update(todos).set(data).where(eq(todos.id, data.id))
+    throw redirect({ to: "/" })
+})
+
+
+export function TodoForm({ todo }: {
+    todo?: {
+        name: string
+        id: string
+    }
+}) {
     const nameRef = useRef<HTMLInputElement>(null)
     const [isloading, setIsLoading] = useState<boolean>(false)
     const addTodoFn = useServerFn(addTodo)
+    const updateTodoFn = useServerFn(updateTodo)
 
     async function handleSubmit(e: FormEvent) {
         e.preventDefault()
         const name = nameRef.current?.value
         if (!name) return
         setIsLoading(true)
-        await addTodoFn({ data: { name } })
+        if (todo == null) {
+
+            await addTodoFn({ data: { name } })
+        } else {
+            await updateTodoFn({ data: { name, id: todo.id } })
+        }
         setIsLoading(false)
 
     }
     return <form onSubmit={handleSubmit} className="flex gap-2">
-        <Input autoFocus ref={nameRef} placeholder="Enter your new todo..." className="flex-1" aria-label="Name" />
+        <Input autoFocus ref={nameRef} placeholder="Enter your new todo..." className="flex-1" aria-label="Name" defaultValue={todo?.name} />
         <Button type="submit" disabled={isloading}>
-            <LoadingSwap className="flex gap-2 items-center" isLoading={isloading}><PlusIcon /> Add</LoadingSwap>
+            <LoadingSwap className="flex gap-2 items-center" isLoading={isloading}>
+                {todo == null ? (
+                    <>
+                        <PlusIcon /> Add
+                    </>
+                ) : (
+                    'Update'
+                )}
+            </LoadingSwap>
         </Button>
     </form>
 }
