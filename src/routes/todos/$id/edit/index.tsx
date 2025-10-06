ab1985f3bd8f5178eeed6e7a7b0154d5405199f1
Himanish kochhar <himanish.kochhar@gmail.com>
2025-10-06 09:45:52 +0530

    added the loaderfunction to get fetch the todo detail from db and to be passed this todo in todoform

--- src/routes/todos/$id/edit/index.tsx
+++ src/routes/todos/$id/edit/index.tsx
@@ -1,15 +1,27 @@
 import { TodoForm } from '@/components/todo-form'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { db } from '@/db'
+import { todos } from '@/db/schema'
+import { createFileRoute, Link, notFound } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+import { eq } from 'drizzle-orm'
 import { ArrowLeftIcon } from 'lucide-react'
 
-
+const loaderFn = createServerFn({ method: "GET" }).inputValidator((data: { id: string }) => data).handler(async ({ data }) => {
+    const todo = await db.query.todos.findFirst({
+        where: eq(todos.id, data.id)
+    })
+    if (todo == null) throw notFound()
+    return todo
+})
 export const Route = createFileRoute('/todos/$id/edit/')({
     component: RouteComponent,
+    loader: ({ params }) => loaderFn({ data: params })
 })
 
 function RouteComponent() {
+    const todo = Route.useLoaderData()
     return (
         <div className="container space-y-2">
             <Button asChild variant="ghost" size="sm" className="text-muted-foreground">
@@ -17,11 +29,11 @@ function RouteComponent() {
             </Button>
             <Card>
                 <CardHeader>
-                    <CardTitle>Edit Todo</CardTitle>
+                    <CardTitle>Edit Todo - {todo.name}</CardTitle>
                     <CardDescription>Update the details of your todo item</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <TodoForm />
+                    <TodoForm todo={todo} />
                 </CardContent>
             </Card>
         </div>
